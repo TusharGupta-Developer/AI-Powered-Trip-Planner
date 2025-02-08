7aede7017a3f5d37afd5da847b0ad214cfd91a01
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-02-08 21:46:48 +0530

    Import the Input component from shadcn/ui

--- src/components/features/trip/CreateTrip.jsx
+++ src/components/features/trip/CreateTrip.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { Input } from '@/components/ui/input';
 
 
 function CreateTrip() {
@@ -9,21 +10,27 @@ function CreateTrip() {
       <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
       <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner generate a customzed intinerary based on your preferences.</p>
 
-      <div className='mt-10'>
+      <div className='mt-20 flex flex-col gap-9'>
         <div>
           <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
           <GooglePlacesAutocomplete
             apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
             selectProps={{
               place,
-              onChange: (v) =>{
+              onChange: (v) => {
                 setPlace(v);
                 console.log(v)
               }
             }}
           />
         </div>
+
+        <div>
+          <h2 className='text-xl my-3 font-medium'>How many days are you plannig your trip?</h2>
+          <Input placeholder="Ex.3" type="number" />
+        </div>
       </div>
+
     </div>
   )
 }
