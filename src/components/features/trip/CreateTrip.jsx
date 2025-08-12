a258309c57fc441b43d236d334214f41ed541d82
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-08-12 19:13:30 +0530

    Maked CreateTrip.jsx

--- src/components/features/trip/CreateTrip.jsx
+++ src/components/features/trip/CreateTrip.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { Input } from '@/components/ui/input';
 import { SelectBudgetOptions, SelectTravelsLists } from '@/constants/options';
@@ -7,6 +7,31 @@ import { Button } from '@/components/ui/button';
 
 function CreateTrip() {
   const [place, setPlace] = useState();
+  const [formData, setFormData] = useState([]);
+
+  const handleInputChange = (name, value) => {
+
+    // if (name == 'noOfDays' && value > 5) {
+    //   console.lol("Please enter Trip Days less than 5")
+    // }
+
+    setFormData({
+      ...formData,//It preserves the existing data in formData and It updates only the specified field (name) with the new value.
+      [name]: value
+    })
+  }
+
+  useEffect(() => {
+    console.log(formData)
+  }, [formData])
+
+  const OnGenerateTrip = () => {
+    if (formData?.noOfDays > 5) {
+      return;
+    }
+    console.log(formData)// if condition noOfDays<5 then i able to console the formdata. 
+  }
+
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preferences🏝️🏔️🌴</h2>
@@ -22,7 +47,8 @@ function CreateTrip() {
               place,
               onChange: (v) => {
                 setPlace(v);
-                console.log(v)
+                console.log(v);
+                handleInputChange('location', v)
               }
             }}
           />
@@ -30,7 +56,9 @@ function CreateTrip() {
 
         <div>
           <h2 className='text-xl my-3 font-medium'>How many days are you plannig your trip?</h2>
-          <Input placeholder="Ex.3" type="number" />
+          <Input placeholder="Ex.3" type="number"
+            onChange={(e) => handleInputChange('noOfDays', e.target.value)}
+          />
         </div>
 
         {/* Budget selection and travel companion selection grids with interactive cards */}
@@ -38,7 +66,8 @@ function CreateTrip() {
           <h2 className='text-xl my-3 font-medium'>What is your Budget?</h2>
           <div className='grid grid-cols-3 gap-5 mt-5'>
             {SelectBudgetOptions.map((item, index) => (
-              <div key={index} className='p-4 border rounded-lg hover:shadow cursor-pointer'>
+              <div key={index} className={`p-4 border rounded-lg hover:shadow cursor-pointer ${formData?.budget == item.title && 'shadow-lg border-black'}`} // use ? to safely access formData.budget without throwing an error if formData is null or undefined.
+                onClick={(e) => handleInputChange("budget", item.title)}>
                 <h2 className='text-4xl mb-1'>{item.icon}</h2>
                 <h2 className='font-bold text-lg'>{item.title}</h2>
                 <h2 className='text-sm text-gray-500'>{item.desc}</h2>
@@ -50,7 +79,8 @@ function CreateTrip() {
           <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on your next adventure?</h2>
           <div className='grid grid-cols-3 gap-5 mt-5'>
             {SelectTravelsLists.map((item, index) => (
-              <div key={index} className='p-4 border rounded-lg hover:shadow cursor-pointer'>
+              <div key={index} className={`p-4 border rounded-lg hover:shadow cursor-pointer ${formData?.traveler == item.people && 'shadow-lg border-black'}`}
+                onClick={(e) => handleInputChange("traveler", item.people)}>
                 <h2 className='text-4xl mb-1'>{item.icon}</h2>
                 <h2 className='font-bold text-lg'>{item.title}</h2>
                 <h2 className='text-sm text-gray-500'>{item.desc}</h2>
@@ -62,7 +92,7 @@ function CreateTrip() {
       </div>
 
       <div className='my-10 flex justify-end'>
-        <Button>Generate Trip</Button>
+        <Button onClick={OnGenerateTrip}>Generate Trip</Button>
       </div>
 
     </div>
