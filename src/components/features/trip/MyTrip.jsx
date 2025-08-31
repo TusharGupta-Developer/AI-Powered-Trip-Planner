e3783eb380ee584fafc50db76cab97ed4379d442
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-08-31 23:17:12 +0530

    feat(UserMyTripCardItem.jsx): Added Photo API setup same as in InfoSection and debugged issue in MyTrip

--- src/components/features/trip/MyTrip.jsx
+++ src/components/features/trip/MyTrip.jsx
@@ -20,11 +20,11 @@ function MyTrip() {
       naviagte("/")
       return;
     }
-
     //Get multiple documents from a collection
     const q = query(collection(db, "AITrips"), where("userEmail", "==", user?.email));
-
+    
     const querySnapshot = await getDocs(q);
+    setUserTrips([])
     // querySnapshot.forEach((doc) => {
     //   // doc.data() is never undefined for query doc snapshots
     //   console.log(doc.id, " => ", doc.data());
@@ -41,7 +41,7 @@ function MyTrip() {
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>My Trips</h2>
 
-      <div className='grid grid-cols-2 md:grid-cols-3 gap-5 mt-7'>
+      <div className='grid grid-cols-2 md:grid-cols-3 gap-5 mt-10'>
         {userTrips.map((trip, index) => (
           <div key={index}>
             < UserMyTripCardItem trip={trip} />
--- src/components/features/trip/UserMyTripCardItem.jsx
+++ src/components/features/trip/UserMyTripCardItem.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import { GetPlaceDetail, PHOTP_REF_URL } from '@/service/GlobalApi';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 function UserMyTripCardItem({ trip }) {
+
+    const [photo, setPhoto] = useState()
+    // console.log(trip)
+
+    useEffect(() => {
+        if (trip?.userSelection?.location?.label) { // Self: If used here because when page is load then instantly GetPlacePhoto(); is called but at that time data have no value as trip?.userSelection?.location?.label not run that time so "error", To fix this problem I used if condfiyion that only runs GetPlacePhoto(); when trip?.userSelection?.location?.label have value.
+            GetPlacePhoto();
+        }
+    }, [trip]);
+
+    const data = {
+
+        textQuery: trip?.userSelection?.location?.label
+    };
+
+    const GetPlacePhoto = async () => {
+        try {
+            const resp = await GetPlaceDetail(data);
+            console.log(resp.data);
+            console.log(resp.data.places[0].photos[3]);
+            console.log(resp.data.places[0].photos[3].name);
+            const PhotoUrl = PHOTP_REF_URL.replace('{NAME}', resp.data.places[0].photos[3].name)
+            // console.log(PhotoUrl)
+            setPhoto(PhotoUrl)
+
+        } catch (err) {
+            console.error("Google Places API Error:", err.response?.data || err.message);
+        }
+    };
+
     return (
-        <div>
-            <img src="/placeholder.jpg" alt="" className="object-cover rounded-xl" />
-        </div>
+        <Link to={'/create-trip/view-trip/' + trip?.Id}>
+            <div className='hover:scale-105 transition-all' >
+                <div className="w-50 h-60">
+                    <img
+                        src={photo}
+                        alt=""
+                        className="w-full h-full object-cover rounded-xl"
+                    />
+                </div>
+
+                <div>
+                    <h2 className='font-bold text-lg'>{trip?.userSelection?.location?.label}</h2>
+                    <h2 className='text-sm text-gray-500' >{trip?.userSelection?.noOfDays} Days trip with {trip?.userSelection?.budget} Budget</h2>
+                </div>
+            </div>
+        </Link>
     )
 }
 
