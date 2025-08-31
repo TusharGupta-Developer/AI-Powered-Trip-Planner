be7c4774060993385336d138965653e32c69a930
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-08-31 18:36:46 +0530

    feat(MyTrip): Created UserMyTripCardItem.jsx

---
+++ src/components/features/trip/UserMyTripCardItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserMyTripCardItem({ trip }) {
+    return (
+        <div>
+            <img src="/placeholder.jpg" alt="" className="object-cover rounded-xl" />
+        </div>
+    )
+}
+
+export default UserMyTripCardItem
